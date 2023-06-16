262cf3cce1c40deeedb5bb36da2529d725e7f37d
Yutaro Ohno <yutaro.ono.418@gmail.com>
2023-06-16 18:34:15 +0900

    patch: autostart
    https://github.com/djpohly/dwl/wiki/autostart

--- config.def.h
+++ config.def.h
@@ -7,6 +7,12 @@ static const float focuscolor[]            = {1.0, 0.0, 0.0, 1.0};
 /* To conform the xdg-protocol, set the alpha to zero to restore the old behavior */
 static const float fullscreen_bg[]         = {0.1, 0.1, 0.1, 1.0};
 
+/* Autostart */
+static const char *const autostart[] = {
+        "wbg", "/path/to/your/image", NULL,
+        NULL /* terminate */
+};
+
 /* tagging - tagcount must be no greater than 31 */
 static const int tagcount = 9;
 
